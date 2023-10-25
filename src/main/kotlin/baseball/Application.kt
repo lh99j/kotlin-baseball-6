f66ab02f7b1b89f7f55dae83899191a3a63b1017
lh99j <grand2181@gmail.com>
2023-10-25 18:07:05 +0900

    feat: 게임 시작 문구 출력 기능 구현

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -2,9 +2,12 @@ package baseball
 
 import baseball.Model.Computer
 import baseball.View.InputView
+import baseball.View.OutputView
 
 fun main() {
     val computer = Computer()
     val inputView = InputView()
+    val outputView = OutputView()
+    outputView.printStartMessage()
     inputView.inputUserNumber()
 }
---
+++ src/main/kotlin/baseball/View/OutputView.kt
@@ -0,0 +1,7 @@
+package baseball.View
+
+class OutputView {
+    fun printStartMessage() {
+        println("숫자 야구 게임을 시작합니다.")
+    }
+}
